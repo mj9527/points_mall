36d45c39625b8c0bb2908b5671e3124839fda769
mjzheng <mjzheng@tencent.com>
2020-03-25 11:19:55 +0800

    [需求] 协议生成重构

--- gen_go.sh
+++ gen_go.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-protoc --go_out=plugins=grpc:./ github.com/mj9527/points_mall/points_mall.proto
+protoc --go_out=plugins=grpc:./ ./points_mall.proto
+
+cp github.com/mj9527/points_mall/points_mall.pb.go ./
+
